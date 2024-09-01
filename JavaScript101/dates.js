2a6fe0e3726bc1bdceb87d04c90607213cb784cc
T Shivani <96899982+shivani-tripurari@users.noreply.github.com>
2024-09-01 12:11:25 +0530

    add dates.js

---
+++ JavaScript101/dates.js
@@ -0,0 +1,7 @@
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.toString());
+console.log(newDate.toISOString());
+console.log(newDate.getMinutes());
+console.log(Date.now());
+console.log(Math.floor(Date.now()/1000));
